8ffb9b24fb0432b44f421adeaa0cced63665e60f
Gavin Vanderveen <25GAV01@stu.cpsk12.org>
2024-01-29 14:19:33 -0600

    Background partial implementation

---
+++ src/Background.cpp
@@ -0,0 +1,12 @@
+#include "Background.h"
+
+Background::Background(std::string file, sf::Vector2f pos, sf::Vector2f velocity)
+{
+	vel = velocity;
+
+	if (!texture.loadFromFile(file))
+		std::cout << "Background file could not load\n";
+	
+	back.setTexture(texture);
+	back.setPosition(pos);
+}
