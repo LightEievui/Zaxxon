f8f01cdffabe87fca7a35fe2eddd972d9c0e825d
Gavin Vanderveen <25GAV01@stu.cpsk12.org>
2024-01-31 13:37:11 -0600

    Background relatively finalized, window scaling added

--- src/Background.h
+++ src/Background.h
@@ -6,14 +6,17 @@
 class Background
 {
 public:
-	Background(std::string, float scale, sf::Vector2f = sf::Vector2f(0, 0), sf::Vector2f = sf::Vector2f(0, 0));
+	Background();
 	~Background();
 
+	void create(std::string, float scale, sf::Vector2f = sf::Vector2f(0, 0), sf::Vector2f = sf::Vector2f(0, 0));
 	void setPosition(sf::Vector2f);
 	void setTexture(std::string);
 	void setVelocity(sf::Vector2f);
 	void drawBackground(sf::RenderWindow&);
 	void reset();
+
+	bool backgroundFinished();
 
 private:
 	sf::Texture texture;
