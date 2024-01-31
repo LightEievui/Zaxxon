1c8e4bf3e535fd13b7aa78a88bed52f9025bc4ca
Gavin Vanderveen <25GAV01@stu.cpsk12.org>
2024-01-31 13:37:11 -0600

    Background relatively finalized, window scaling added

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include "Game.h"
 
+const double scale = 3;
+
 
 Game::Game()
 {
-	
+    
 }
 
 
@@ -16,9 +18,12 @@ Game::~Game()
 
 void Game::run()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "Zaxxon");
+    //added to constructor so that it is not created every frame
+    sf::RenderWindow window(sf::VideoMode(256 * scale, 224 * scale), "Zaxxon");
     //Set frame rate limit to smooth out
     window.setFramerateLimit(60);
+
+    background.create("Image.png", scale, sf::Vector2f(0, window.getSize().y), sf::Vector2f(-.8 * scale * gameSpeed, .4 * scale * gameSpeed));
 
     while (window.isOpen())
     {
@@ -32,5 +37,12 @@ void Game::run()
         window.clear();
         background.drawBackground(window);
         window.display();
+
+        //resets the background when image is off of screen.
+        if (background.backgroundFinished())
+        {
+            background.reset();
+            gameSpeed += .2;
+        }
     }
 }
