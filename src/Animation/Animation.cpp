5791b611ed7d0d3cf4844aba9c2bae476b2dff76
Eievui <62132046+LightEievui@users.noreply.github.com>
2024-01-31 15:47:43 -0600

    Basic animation prototype
    Apparently I don't have the time to test, have something else to do this study hall, I'll come back to this

---
+++ src/Animation/Animation.cpp
@@ -0,0 +1,33 @@
+#include "Animation.h"
+
+
+Animation::Animation()
+{
+	// Setup death explosion frames
+	frames[0] = sf::IntRect(184, 144, 31, 31);
+	frames[1] = sf::IntRect(224, 155, 31, 20);
+}
+
+
+Animation::~Animation()
+{
+
+}
+
+
+void Animation::run(sf::Sprite sprite, Anim anim) // TODO: Make animation length fit perfectly, currently estimated
+{
+	timer.restart();
+
+	switch (anim)
+	{
+	case CHARACTER_DEATH:
+		while (timer.getElapsedTime().asSeconds() < 2)
+		{
+			int current = static_cast<int>(std::round(timer.getElapsedTime().asSeconds() * 2)) % 2;
+			sprite.setTextureRect(frames[current]);
+		}
+
+		break;
+	}
+}
