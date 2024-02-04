7cb0503cbd3b166cae77579fbf23df080c2bd8f7
Eievui <62132046+LightEievui@users.noreply.github.com>
2024-02-04 16:02:53 -0600

    Scaling

--- src/Background/Background.h
+++ src/Background/Background.h
@@ -9,7 +9,7 @@ public:
 	Background();
 	~Background();
 
-	void create(std::string, float scale, sf::Vector2f = sf::Vector2f(0, 0), sf::Vector2f = sf::Vector2f(0, 0));
+	void create(std::string, sf::Vector2f = sf::Vector2f(0, 0), sf::Vector2f = sf::Vector2f(0, 0));
 	void setPosition(sf::Vector2f);
 	void setTexture(std::string);
 	void setSpeed(float);
