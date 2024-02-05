35fb8d42d86ccf71ec8e2437291669100a2194a5
Eievui <62132046+LightEievui@users.noreply.github.com>
2024-02-05 12:17:37 -0600

    Return to player sprite

--- src/Animation/Animation.cpp
+++ src/Animation/Animation.cpp
@@ -28,11 +28,14 @@ void Animation::run(sf::Sprite& sprite, Anim anim)
 }
 
 
+// TODO: Not the real character death
 void Animation::fCHARACTER_DEATH(sf::Sprite& sprite)
 {
 	while (timer.getElapsedTime().asSeconds() < 2)
 	{
-		int current = static_cast<int>(std::round(timer.getElapsedTime().asSeconds() * 2)) % 2;
+		int current = static_cast<int>(timer.getElapsedTime().asSeconds() * 2) % 2;
 		sprite.setTextureRect(frames[current]);
 	}
+
+	sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
 }
