81fa8dfcca85dde2194a14aa7124ce24241747dd
Ninjune <enderknight537@gmail.com>
2024-02-08 14:20:17 -0600

    work on height meter, fix res

--- src/GUI/HMSection/HMSection.cpp
+++ src/GUI/HMSection/HMSection.cpp
@@ -34,26 +34,80 @@ int HMSection::getSection()
 	return section;
 }
 
-
 void HMSection::progress(int stage)
 {
 	int change;
+
+	if (stage >= 0)
+	{
+		switch (this->type)
+		{
+		case TOP:
+		case LINE:
+			change = 16 - stage;
+			setSection(change); // full 9-16 to 5-8: blank to full so 8-5 then 16-9: full to blank
+			break;
+		case OPEN:
+			change = 24 - stage;
+			setSection(change); // 17-24 to 0-3: blank to full so 3-0 then 24-17: full to blank
+			break;
+		case BOTTOM:
+			break;
+		}
+	}
+	else
+	{
+		switch (this->type)
+		{
+		case TOP:
+		case LINE:
+			change = 8+stage+1;
+			setSection(change); // 
+			break;
+		case OPEN:
+			change = 3+stage+1;
+			setSection(change); // 
+			break;
+		case BOTTOM:
+			break;
+		}
+	}
+	
+}
+
+
+void HMSection::fill()
+{
+	switch (this->type)
+	{
+	case LINE:
+		setSection(8);
+		break;
+	case OPEN:
+		setSection(3);
+		break;
+	case TOP:
+		setSection(5);
+	case BOTTOM:
+		setSection(4);
+		break;
+	}
+}
+
+
+void HMSection::empty()
+{
 	switch (this->type)
 	{
 	case TOP:
 	case LINE:
-		change = 8 - stage;
-		if (change < 5)
-			change = (change - 4) + 16;
-		setSection(change); // full 9-16 to 5-8: blank to full so 8-5 then 16-9: full to blank
+		setSection(9);
 		break;
 	case OPEN:
-		change = 3 - stage;
-		if (change < 0)
-			change = (change+1) + 24;
-		setSection(change); // 17-24 to 0-3: blank to full so 3-0 then 24-17: full to blank
+		setSection(17);
 		break;
 	case BOTTOM:
+		setSection(4);
 		break;
 	}
 }
--- src/GUI/HMSection/HMSection.h
+++ src/GUI/HMSection/HMSection.h
@@ -13,9 +13,16 @@ public:
 		BOTTOM
 	};
 
+	/*
+		Changes the selection by a certain amount. Higher stage: less full.
+		Stage must be between 0-7.
+	*/
 	void progress(int stage);
 	void setSection(int sect);
 	void setType(Type type);
+	void fill();
+	void empty();
+
 	int getSection();
 private:
 	int section;
