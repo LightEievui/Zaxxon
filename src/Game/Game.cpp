be22ced8e88e08c209ec0111e2495cd7e91f2c0c
Gavin Vanderveen <25GAV01@stu.cpsk12.org>
2024-02-20 14:21:23 -0600

    Fixed "some" obstacle issues from merge

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -46,8 +46,8 @@ void Game::run()
     generateObstacles(&spriteSheet);
 
     //Testing for gavin
-    obstacles.push_back(new Obstacle);
-    obstacles.at(0)->create(sf::Vector3f(-120, 135.6, -700), &spriteSheet, 10, 1);
+    obstacles.push_back(new Obstacle(sf::Vector3f(-120, 135.6, -700), &spriteSheet, 10, 1));
+    //obstacles.at(0)->create(sf::Vector3f(-120, 135.6, -700), &spriteSheet, 10, 1);
 
     GUI gui(&spriteSheet);
 
