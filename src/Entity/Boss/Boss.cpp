daeae6c7778ac869d5bd2bbe38760d511f30662b
Eievui <62132046+LightEievui@users.noreply.github.com>
2024-05-02 13:37:56 -0500

    Delete nullptr
    If you delete on nullptr it will ignore it, so nullptr checks on delete aren't needed

--- src/Entity/Boss/Boss.cpp
+++ src/Entity/Boss/Boss.cpp
@@ -35,8 +35,7 @@ Boss::Boss(sf::Vector3f start, Entity* target, sf::Texture* bossSheet, sf::Textu
 /// </summary> 
 Boss::~Boss() 
 {
-	if (missile != nullptr)
-		delete missile;
+	delete missile;
 }
 
 
